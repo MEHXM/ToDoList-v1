f35a207bc01a2a577f16e66551b2d0b5be1712ad
MEHXM <mehul909.v@gmail.com>
2019-11-04 23:51:47 +0530

    Complete Frontend and Backend

--- app.js
+++ app.js
@@ -4,16 +4,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 let items = ["Buy food" , "Cook food" ,"Eat Food"];
+let work = [];
 
 const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.post("/",function (req,res) {
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+    if(req.body.list==="Work"){
+        work.push(item);
+        res.redirect("/work");
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
 })
 
 app.get("/",function (req,res) {
@@ -24,9 +32,18 @@ app.get("/",function (req,res) {
         month:"long"
     }
     let day = today.toLocaleDateString("en-US",options);
-    res.render("list", {kindOfDay:day ,newItem:items});
+    res.render("list", {listTitle:day ,newItem:items});
     
 });
+
+app.get("/work",function (req,res) {
+    day = "Work";
+    res.render("list", {listTitle:day ,newItem:work});
+});
+
+app.get("/about",function (req,res) {
+    res.render("about");
+})
 
 app.listen(3000,function () {
     console.log("Server started on port 3000");
